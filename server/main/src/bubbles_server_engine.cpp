665ccb8e385a51204e2c0e43fe4ebeb32a691b5d
Valentin Palade <vipalade@gmail.com>
2017-11-17 11:37:46 +0200

    work: fixing bubbles server

--- server/main/src/bubbles_server_engine.cpp
+++ server/main/src/bubbles_server_engine.cpp
@@ -265,7 +265,7 @@ void Engine::onMessage(
             rcon_sender.saveLastEvent(*_rrecv_msg_ptr);
             //TODO: set:
             //_rrecv_msg_ptr->connection_id
-
+            _rrecv_msg_ptr->clearHeader(); // cumbersome - for now!
             _rrecv_msg_ptr->event_stub.sender_rgb_color = rcon_sender.rgb_color;
 
             for(size_t i = 0; i < room.connections.size(); ++i){
@@ -274,7 +274,6 @@ void Engine::onMessage(
 
                 if(i != rcon_data.room_entry_index and rcon.canAcceptEventsNotification(d.config, *_rrecv_msg_ptr, rcon_data.room_entry_index)){
                     rcon.pending_count += (1 + _rrecv_msg_ptr->event_stub.events.size());
-                    _rrecv_msg_ptr->clearHeader(); // cumbersome - for now!
                     _rctx.service().sendMessage(rcon.id, _rrecv_msg_ptr, {frame::mpipc::MessageFlagsE::Synchronous});
                 }else if(i != rcon_data.room_entry_index){
                     idbg(_rctx.recipientId()<<" not sent to "<<i<<" pending count = "<<rcon.pending_count);
